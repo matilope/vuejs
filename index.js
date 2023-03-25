2f0206eb654f584b3cd25a9804dc25dded55f4ab
Matias <mati02171996@gmail.com>
2023-03-24 23:38:45 -0300

    practicas con vue

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        message: "APP CON VUE",
+        mostrar: true,
+        codigo: "<span style='color: blue;'>SPAN</span>",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4940ymTFvsQAB_HHewW-nJbo-z6QX3tN0WFmniGf0&s",
+        elegido: false
+    }
+})
